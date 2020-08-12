b190a988a47a3973e179b7c7256360b3d2518abd
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-08-12 12:27:02 +0100

    specs fixed

--- spec/features/listings_page_spec.rb
+++ spec/features/listings_page_spec.rb
@@ -1,9 +1,9 @@
 feature "The listings page" do
   scenario "has a list of houses" do
+    Listing.create(address: "house 1", description: "lovely house")
+    
     visit('/')
     click_button("Listings")
-    expect(page).to have_content("house 1")
-    expect(page).to have_content("house 2")
-    expect(page).to have_content("house 3")
+    expect(page).to have_content("house 1, lovely house")
   end
 end
