d6b9acebddeaaef1a655c9635fa76bac91f44315
Harry LB <harry.lingardbright@gmail.com>
2020-08-12 14:51:01 +0100

    user.create method added

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,22 @@
+require_relative 'database_connection'
+
 class User
+  attr_reader :id, :username, :email
+
+  def initialize(id:, username:, email:)
+    @id = id
+    @email = email
+    @username = username
+  end
+
+  def self.create(username:, password:, email:)
+    result = DatabaseConnection.query("INSERT INTO users (username, password, email) 
+      VALUES('#{username}', '#{password}', '#{email}') 
+      RETURNING id, username, email;")
+      
+    User.new(id: result[0]['id'], 
+      username: result[0]['username'],
+      email: result[0]['email']
+    )
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,15 @@
 require 'user'
 describe User do
-describe '.creat' do
+  describe '.create' do
+    it 'creates a new user' do
+      user = User.create(username: 'testuser', password: 'test123', email: 'test@testface.com')
+      
+      persisted_data = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{user.id}';")
+      
+      expect(user).to be_a User
+      expect(user.id).to eq persisted_data.first['id']
+      expect(user.email).to eq 'test@testface.com'
+      expect(user.username).to eq 'testuser'
+    end
   end 
 end
