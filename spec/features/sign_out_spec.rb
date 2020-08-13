83cf17aea1f8816b43d83f0a2751073bd61adf0a
Harry LB <harry.lingardbright@gmail.com>
2020-08-13 15:44:58 +0100

    ref

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,11 +1,11 @@
 feature 'sign_out' do
   scenario 'there is a button to sign_out'do
-  User.create(username: 'test_username', password: 'test_password', email: 'test@test.com')
-  visit '/'
-  click_button 'Login'
-  fill_in('username', with: 'test_username')
-  fill_in('password', with: 'test_password')
-  click_button 'Login'
-  expect(page).to have_button 'Sign Out'
+    User.create(username: 'test_username', password: 'test_password', email: 'test@test.com')
+    visit '/'
+    click_button 'Login'
+    fill_in('username', with: 'test_username')
+    fill_in('password', with: 'test_password')
+    click_button 'Login'
+    expect(page).to have_button 'Sign Out'
   end
 end
